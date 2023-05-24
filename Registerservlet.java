d1d8b421aeeee7b0779afdf118275ad564dcd7bc
sneha-choudhary-20 <124805494+sneha-choudhary-20@users.noreply.github.com>
2023-05-24 15:54:21 +0530

    Completed the website

---
+++ Registerservlet.java
@@ -0,0 +1,80 @@
+package com.grocery.shop.servlets;
+import com.grocery.shop.dao.UserDao;
+import com.grocery.shop.entities.User;
+import com.grocery.shop.helper.ConnectionProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+public class Registerservlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {         
+            // fetch all form data
+              String name=request.getParameter("user_name");
+              String email=request.getParameter("user_email");
+              String password=request.getParameter("user_password");
+              String phone=request.getParameter("user_phone");
+              String address=request.getParameter("user_address");           
+              //create user object and set all data to that object
+              User user=new User(name , email , password , phone , address, "normal");          
+              // create userdao object
+              UserDao dao=new UserDao(ConnectionProvider.getConnection());
+              if(dao.saveUser(user)){
+                //save....
+               response.sendRedirect("login.jsp");
+               return;
+               }
+             else
+              {
+                //error  
+                HttpSession httpSession = request.getSession();
+                httpSession.setAttribute("message", " User already exists  !!  Try with another one ");
+                response.sendRedirect("register.jsp");
+                return;
+              }
+           }         
+        }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ RemoveProductCartServlet.java
@@ -0,0 +1,71 @@
+package com.grocery.shop.servlets;
+import com.grocery.shop.dao.CartDao;
+import com.grocery.shop.helper.ConnectionProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+public class RemoveProductCartServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           int pid=Integer.parseInt(request.getParameter("pid"));
+           int uid=Integer.parseInt(request.getParameter("uid"));
+           int cid=Integer.parseInt(request.getParameter("cid"));
+           CartDao dao=new CartDao(ConnectionProvider.getConnection());        
+           boolean f=dao.deleteProduct(pid,uid,cid);          
+           if(f)
+           {
+               HttpSession httpSession = request.getSession();
+               httpSession.setAttribute("message", " Product Removed from Cart....");
+               response.sendRedirect("checkout.jsp");
+               return;
+           }else
+           {
+               out.println("Something went wrong...");
+           }               
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
